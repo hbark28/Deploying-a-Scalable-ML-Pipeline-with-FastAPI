5e301d4f04345c075af0f7dcc03adc40c064b438
Heather Barker <hbark28@wgu.edu>
2024-02-21 07:58:05 -0700

    corrected format in model file

--- ml/model.py
+++ ml/model.py
@@ -149,7 +149,7 @@ def performance_on_categorical_slice(
     """
 
     # TODO: implement the function
-        X_slice, y_slice, _, _ = process_data(
+    X_slice, y_slice, _, _ = process_data(
         data,
         categorical_features=categorical_features,
         label=label,
